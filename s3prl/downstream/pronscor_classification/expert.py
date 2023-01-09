0edfaff2fb45242d2ee49ae588e74f740c85e735
Jazmin <jazmin.vidald@gmail.com>
2023-01-09 13:05:24 -0300

    added pronscor_classification folder

---
+++ s3prl/downstream/pronscor_classification/expert.py
@@ -0,0 +1,234 @@
+###############
+# IMPORTATION #
+###############
+import os
+import math
+import random
+from IPython import embed
+from collections import defaultdict
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from .model import ConvBank
+from .dataset import PronscorDataset
+from .train_utils import format_labels #, criterion 
+import numpy as np
+
+
+class DownstreamExpert(nn.Module):
+    """
+    Adapted from phone linear expert
+
+    Used to handle downstream-specific operations
+    eg. downstream forward, metric computation, contents to log
+    """
+
+    def __init__(self, upstream_dim, downstream_expert, expdir, **kwargs):
+        super(DownstreamExpert, self).__init__()
+        self.upstream_dim = upstream_dim
+        self.datarc = downstream_expert['datarc']
+        self.modelrc = downstream_expert['modelrc']
+
+        #self.phone_weights = get_phone_weights_as_torch(config['downstream_expert']['datarc']['phone_weights'])
+        #self.npc = config['downstream_expert']['datarc']['npc']
+
+        self.train_dataset = PronscorDataset(
+            'train', self.datarc['train_batch_size'], **self.datarc)
+        self.dev_dataset = PronscorDataset(
+            'dev', self.datarc['eval_batch_size'], **self.datarc)
+        self.test_dataset = PronscorDataset(
+            'test', self.datarc['eval_batch_size'], **self.datarc)
+        # self.model = Model(input_dim=self.upstream_dim,
+        #    output_class_num=self.train_dataset.class_num, **self.modelrc)
+        self.objective = nn.BCEWithLogitsLoss()
+
+        self.logging = os.path.join(expdir, 'log.log')
+        self.best = defaultdict(lambda: 0)
+
+        # delattr(self, 'model')
+        model_cls = eval(self.modelrc['select'])
+        model_conf = self.modelrc[self.modelrc['select']]
+        self.model = model_cls(
+            self.upstream_dim, output_class_num=self.train_dataset.class_num, **model_conf)
+
+    def _get_train_dataloader(self, dataset):
+        return DataLoader(
+            dataset, batch_size=1,  # for bucketing
+            shuffle=True, num_workers=self.datarc['num_workers'],
+            drop_last=False, pin_memory=True, collate_fn=dataset.collate_fn
+        )
+
+    def _get_eval_dataloader(self, dataset):
+
+        return DataLoader(
+            dataset, batch_size=1,  # for bucketing
+            shuffle=False, num_workers=self.datarc['num_workers'],
+            drop_last=False, pin_memory=True, collate_fn=dataset.collate_fn
+        )
+
+    """
+    Datalaoder Specs:
+        Each dataloader should output in the following format:
+
+        [[wav1, wav2, ...], your_other_contents1, your_other_contents2, ...]
+
+        where wav1, wav2 ... are in variable length
+        each wav is torch.FloatTensor in cpu with dim()==1 and sample_rate==16000
+    """
+
+    def get_train_dataloader(self):
+        return self._get_train_dataloader(self.train_dataset)
+
+    def get_dev_dataloader(self):
+        return self._get_eval_dataloader(self.dev_dataset)
+
+    def get_test_dataloader(self):
+        return self._get_eval_dataloader(self.test_dataset)
+
+    # Interface
+    def get_dataloader(self, split):
+        return eval(f'self.get_{split}_dataloader')()
+    
+
+    def _tile_representations(self, reps, factor):
+        """ 
+        Tile up the representations by `factor`.
+        Input - sequence of representations, shape: (batch_size, seq_len, feature_dim)
+        Output - sequence of tiled representations, shape: (batch_size, seq_len * factor, feature_dim)
+        """
+        assert len(reps.shape) == 3, 'Input argument `reps` has invalid shape: {}'.format(
+            reps.shape)
+        tiled_reps = reps.repeat(1, 1, factor)
+        tiled_reps = tiled_reps.reshape(
+            reps.size(0), reps.size(1)*factor, reps.size(2))
+        return tiled_reps
+
+    def _match_length(self, inputs, labels):
+        """
+        Since the upstream extraction process can sometimes cause a mismatch
+        between the seq lenth of inputs and labels:
+        - if len(inputs) > len(labels), we truncate the final few timestamp of inputs to match the length of labels
+        - if len(inputs) < len(labels), we duplicate the last timestep of inputs to match the length of labels
+        Note that the length of labels should never be changed.
+        """
+        input_len, label_len = inputs.size(1), labels.size(-1)
+
+        factor = int(round(label_len / input_len))
+        print(factor)
+        if factor > 1:
+            inputs = self._tile_representations(inputs, factor)
+            input_len = inputs.size(1)
+
+        if input_len > label_len:
+            inputs = inputs[:, :label_len, :]
+        elif input_len < label_len:
+            # (batch_size, 1, feature_dim)
+            pad_vec = inputs[:, -1, :].unsqueeze(1)
+            # (batch_size, seq_len, feature_dim), where seq_len == labels.size(-1)
+            inputs = torch.cat(
+                (inputs, pad_vec.repeat(1, label_len-input_len, 1)), dim=1)
+        return inputs, labels
+
+
+    # Interface
+    def forward(self, split, features, labels, phone_ids, records, npc, **kwargs):
+        """
+        Args:
+            features:
+                list of unpadded features [feat1, feat2, ...]
+                each feat is in torch.FloatTensor and already
+                put in the device assigned by command-line args
+
+            labels:
+                the frame-wise phone labels
+
+            records:
+                defaultdict(list), by appending contents into records,
+                these contents can be averaged and logged on Tensorboard
+                later by self.log_records every log_step
+
+            logger:
+                Tensorboard SummaryWriter, given here for logging/debugging convenience
+                please use f'{prefix}your_content_name' as key name
+                to log your customized contents
+
+            prefix:
+                used to indicate downstream and train/test on Tensorboard
+                eg. 'phone/train-'
+
+            global_step:
+                global_step in runner, which is helpful for Tensorboard logging
+
+        Return:
+            loss:
+                the loss to be optimized, should not be detached
+        """
+        lengths = torch.LongTensor([len(l) for l in labels])
+
+        features = pad_sequence(features, batch_first=True)
+        labels = pad_sequence(labels, batch_first=True, padding_value=-100).to(features.device)
+        features, labels = self._match_length(features, labels)
+        
+        labels = labels.detach().cpu().numpy()
+        phone_ids = pad_sequence(phone_ids, batch_first=True, padding_value=-100).detach().cpu().numpy()
+        
+        predicted = self.model(features)
+        
+        labels2d_list = []
+        for lab, phn in zip(labels, phone_ids):
+            labels_2darray = format_labels(lab, phn)
+            labels2d_list.append(labels_2darray)
+
+        labels2tensor = labels2d_list[0].T
+        for l in labels2d_list[1:]:
+            labels2tensor = np.dstack((labels2tensor,l.T))
+        labels = torch.from_numpy(labels2tensor.T).to(features.device)
+        embed()
+
+        #loss = criterion(predicted, labels, weights=phone_weights, norm_per_phone_and_class=norm_per_phone_and_class, min_frame_count=0)
+        
+        
+        
+        
+        #loss = self.objective(, )
+
+        
+    # interface
+    def log_records(self, split, records, logger, global_step, **kwargs):
+        """
+        Args:
+            records:
+                defaultdict(list), contents already appended
+
+            logger:
+                Tensorboard SummaryWriter
+                please use f'{prefix}your_content_name' as key name
+                to log your customized contents
+
+            prefix:
+                used to indicate downstream and train/test on Tensorboard
+                eg. 'phone/train-'
+
+            global_step:
+                global_step in runner, which is helpful for Tensorboard logging
+        """
+        prefix = f'pronscor/{split}-'
+        average = torch.FloatTensor(records['acc']).mean().item()
+
+        logger.add_scalar(
+            f'{prefix}acc',
+            average,
+            global_step=global_step
+        )
+        message = f'{prefix}|step:{global_step}|acc:{average}\n'
+        save_ckpt = []
+        if average > self.best[prefix]:
+            self.best[prefix] = average
+            message = f'best|{message}'
+            name = prefix.split('/')[-1].split('-')[0]
+            save_ckpt.append(f'best-states-{name}.ckpt')
+        with open(self.logging, 'a') as f:
+            f.write(message)
+
+        return save_ckpt
---
+++ s3prl/downstream/pronscor_classification/train_utils.py
@@ -0,0 +1,130 @@
+
+import torch
+import numpy as np
+import yaml
+
+def format_labels(labels_array, phones_array):
+    '''
+    Function that receives two flat tensors of labels and phone ids
+    and turns them into a quasi one hot encoded (x_X) 2d tensor with
+    labels in the frames of each target phone in the phrase
+    '''
+    idx = np.where(phones_array[:-1] != phones_array[1:])[0]
+    labels_2darray = np.zeros((len(phones_array),40))
+    start = 0
+    for elem in idx:
+        tmp_2darray = np.zeros((len(phones_array),40))
+        end = elem
+        col = phones_array[end]
+        lab = labels_array[end]
+        tmp_2darray[col][start:end+1] = lab
+        labels_2darray += tmp_2darray
+        start = end+1
+    return(labels_2darray)
+
+
+
+#phone_sym2int_dict:  Dictionary mapping phone symbol to integer given a phone list path
+#phone_int2sym_dict:  Dictionary mapping phone integer to symbol given a phone list path
+#phone_int2node_dict: Dictionary mapping phone symbol to the index of the node in the networks's output layer
+#NOTE: The node number in the output layer is not the same as the phone number, as some phones will not be scored
+
+def get_phone_dictionaries(phone_list_path):
+    #Open file that contains list of pure phones
+    phones_list_fh = open(phone_list_path, "r")
+
+    phone_sym2int_dict  = {}
+    phone_int2sym_dict  = {}
+    phone_int2node_dict = {}
+    current_node_index  = 0
+    #Populate the dictionaries
+    for line in phones_list_fh.readlines():
+        line = line.split()
+        phone_symbol = line[0]
+        phone_number = int(line[1])
+        use_phone    = bool(int(line[2]))
+        if use_phone:
+            phone_sym2int_dict[phone_symbol]    = phone_number
+            phone_int2sym_dict[phone_number]    = phone_symbol
+            phone_int2node_dict[phone_number]   = current_node_index
+            current_node_index += 1
+
+    return phone_sym2int_dict, phone_int2sym_dict, phone_int2node_dict
+
+
+def get_phone_weights_as_torch(phone_weights_path):
+    phone_weights_fh = open(phone_weights_path)
+    phone_weights = yaml.safe_load(phone_weights_fh)
+    weights_list = []
+    for phone, weight in phone_weights.items():
+        weights_list.append(weight)
+    phone_weights = weights_list
+    return torch.tensor(phone_weights)
+
+
+
+
+
+'''
+
+def calculate_loss(outputs, mask, labels, phone_weights=None, norm_per_phone_and_class=False, min_frame_count=0):
+    weights = mask *1
+
+    if phone_weights is not None:
+        weights = weights * phone_weights
+
+    if norm_per_phone_and_class:
+        frame_count = torch.sum(mask, dim=[0,1])
+        weights = weights * torch.nan_to_num(1 / frame_count)
+        if min_frame_count > 0:
+            # Set to 0 the weights for the phones with too few cases in this batch
+            weights[:,:,frame_count<min_frame_count] = 0.0
+
+    loss_fn = torch.nn.BCEWithLogitsLoss(reduction='none', weight=weights)
+    
+    return loss_fn(outputs, labels), torch.sum(weights)
+
+
+
+def criterion(batch_outputs, batch_labels, weights=None, norm_per_phone_and_class=False, log_per_phone_and_class_loss=False, phone_int2sym=None, phone_int2node = None, min_frame_count=0):
+
+    batch_labels_for_loss = torch.abs((batch_labels-1)/2)
+
+    loss_pos, sum_weights_pos = calculate_loss(batch_outputs, batch_labels ==  1, batch_labels_for_loss, 
+        phone_weights=weights, norm_per_phone_and_class=norm_per_phone_and_class, min_frame_count=min_frame_count)
+    loss_neg, sum_weights_neg = calculate_loss(batch_outputs, batch_labels == -1, batch_labels_for_loss, 
+        phone_weights=weights, norm_per_phone_and_class=norm_per_phone_and_class, min_frame_count=min_frame_count)
+
+    
+    total_loss = (loss_pos + loss_neg).sum()
+
+    if not norm_per_phone_and_class:
+        total_weights = sum_weights_pos + sum_weights_neg
+        total_loss /= total_weights
+
+    if log_per_phone_and_class_loss:
+
+        pos_phone_loss = torch.sum(loss_pos,dim=[0,1])
+        neg_phone_loss = torch.sum(loss_neg,dim=[0,1])
+        loss_dict = {}
+        for phone_int, phone_sym in phone_int2sym.items():
+            phone_node_index = phone_int2node[phone_int]
+            loss_dict[phone_sym+'+'] = pos_phone_loss[phone_node_index] / weights[phone_node_index] / sum_weights_pos
+            loss_dict[phone_sym+'-'] = neg_phone_loss[phone_node_index] / weights[phone_node_index] / sum_weights_neg
+
+        return total_loss, loss_dict
+    else:
+        return total_loss
+
+
+####
+loss = criterion_fast(outputs, labels, weights=phone_weights, phone_int2sym=phone_int2sym, 
+                        phone_int2node=phone_int2node, norm_per_phone_and_class=norm_per_phone_and_class, min_frame_count=0)
+
+    loss.requires_grad_()
+
+    loss.backward()
+
+    return loss
+
+'''
