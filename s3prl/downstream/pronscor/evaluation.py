fe9c83175851081ab38036953efe6f3e3fd71e47
pablo riera <pablo.riera@gmail.com>
2023-01-10 13:59:23 -0300

    eval

--- s3prl/downstream/pronscor/evaluation.py
+++ s3prl/downstream/pronscor/evaluation.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import random
 import tempfile
 
@@ -10,42 +11,35 @@ from s3prl.downstream.runner import Runner
 import torch
 import numpy as np
 
-ckpt_path = '/home/priera/s3prl/s3prl/result/downstream/test3/states-150000.ckpt'
-ckpt = torch.load(ckpt_path, map_location='cpu')
-ckpt['Args'].device = 'cpu'
-ckpt['Args'].init_ckpt = ckpt_path
 
-runner = Runner(ckpt['Args'], ckpt['Config'])
-
-
-def evaluate(self, split=None, logger=None, global_step=0):
+def evaluate(runner, split=None, logger=None, global_step=0):
     """evaluate function will always be called on a single process even during distributed training"""
 
     # When this member function is called directly by command line
     not_during_training = split is None and logger is None and global_step == 0
     if not_during_training:
-        split = self.args.evaluate_split
+        split = runner.args.evaluate_split
         tempdir = tempfile.mkdtemp()
         logger = SummaryWriter(tempdir)
 
     # fix seed to guarantee the same evaluation protocol across steps
-    random.seed(self.args.seed)
-    np.random.seed(self.args.seed)
-    torch.manual_seed(self.args.seed)
+    random.seed(runner.args.seed)
+    np.random.seed(runner.args.seed)
+    torch.manual_seed(runner.args.seed)
     if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(self.args.seed)
-        with torch.cuda.device(self.args.device):
+        torch.cuda.manual_seed_all(runner.args.seed)
+        with torch.cuda.device(runner.args.device):
             torch.cuda.empty_cache()
 
     # record original train/eval states and set all models to eval
     trainings = []
-    for entry in self.all_entries:
+    for entry in runner.all_entries:
         trainings.append(entry.model.training)
         entry.model.eval()
 
     # prepare data
-    dataloader = self.downstream.model.get_dataloader(split)
-    evaluate_ratio = float(self.config["runner"].get("evaluate_ratio", 1))
+    dataloader = runner.downstream.model.get_dataloader(split)
+    evaluate_ratio = float(runner.config["runner"].get("evaluate_ratio", 1))
     evaluate_steps = round(len(dataloader) * evaluate_ratio)
 
     batch_ids = []
@@ -54,15 +48,25 @@ def evaluate(self, split=None, logger=None, global_step=0):
         if batch_id > evaluate_steps:
             break
 
-        wavs = [torch.FloatTensor(wav).to(self.args.device)
+        wavs = [torch.FloatTensor(wav).to(runner.args.device)
                 for wav in wavs]
         with torch.no_grad():
-            features = self.upstream.model(wavs)
-            features = self.featurizer.model(wavs, features)
-            self.downstream.model(
+            features = runner.upstream.model(wavs)
+            features = runner.featurizer.model(wavs, features)
+            runner.downstream.model(
                 split,
                 features, *others,
                 records=records,
                 batch_id=batch_id,
             )
             batch_ids.append(batch_id)
+
+
+ckpt_path = '/home/priera/s3prl/s3prl/result/downstream/test3/states-150000.ckpt'
+ckpt = torch.load(ckpt_path, map_location='cpu')
+ckpt['Args'].device = 'cpu'
+ckpt['Args'].init_ckpt = ckpt_path
+
+runner = Runner(ckpt['Args'], ckpt['Config'])
+
+evaluate(runner)
